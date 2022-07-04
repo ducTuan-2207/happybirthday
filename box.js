8c920ac9d6bffd3f9dd696703ec2ec27e63c1b78
duc tuan <tuanhypc02@gmail.com>
2022-07-04 23:38:17 +0700

    happy bday

---
+++ box.js
@@ -0,0 +1,40 @@
+var to = "Móm!";
+var gift_url = "https://www.facebook.com/thaor972";
+var gift_image_url = "sn.jpg";
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+function init() {
+  var _giftLink, _giftImg;
+
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if (_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+
+  present.addEventListener(
+    "click",
+    function (e) {
+      present.classList.toggle("open");
+    },
+    false
+  );
+
+  nametag.innerText = to;
+}
+
+init();
